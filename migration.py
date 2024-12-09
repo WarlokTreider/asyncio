4e02d61bd44a3747eb2ba7d9ab65da85c5b52c20
WarlokTreider <163326255+WarlokTreider@users.noreply.github.com>
2024-12-09 22:12:02 +0300

    Update migration.py

--- migration.py
+++ migration.py
@@ -8,24 +8,7 @@ async def create_table():
         database='swapi_characters_db',
         host='localhost'
     )
-    await conn.execute("""
-        CREATE TABLE IF NOT EXISTS characters (
-            id SERIAL PRIMARY KEY,
-            birth_year TEXT,
-            eye_color TEXT,
-            films TEXT,
-            gender TEXT,
-            hair_color TEXT,
-            height TEXT,
-            homeworld TEXT,
-            mass TEXT,
-            name TEXT,
-            skin_color TEXT,
-            species TEXT,
-            starships TEXT,
-            vehicles TEXT
-        );
-    """)
+    await conn.execute()
     print("Table 'characters' created successfully.")
     await conn.close()
 
